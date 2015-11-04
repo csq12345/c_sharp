da2b8859130ed725429c7ef2b8f84a91db337fb2
csq12345 <aqing.123@163.com>
2015-11-04 23:16:47 +0800

    12312321eqweq

--- MyMap/ToolHelper/zoomlevel.cs
+++ MyMap/ToolHelper/zoomlevel.cs
@@ -2,49 +2,58 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Configuration;
 
 namespace ToolHelper
 {
     public static class zoomlevel
     {
+        static System.Configuration.AppSettingsReader ar = new AppSettingsReader();
+        static string url1 = ar.GetValue("url1", typeof(string)).ToString();
+        static string url2 = ar.GetValue("url1", typeof(string)).ToString();
+
+        static string url3 = ar.GetValue("url1", typeof(string)).ToString();
+
+        static string url4 = ar.GetValue("url1", typeof(string)).ToString();
+
         public static Zoom GetLevel(int zoom,MapType mt)
         {
-
-            string url1 = "http://mt0.google.cn/vt?pb=!1m4!1m3!1i@Z!2i@X!3i@Y!2m3!1e0!2sm!3i323191379!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0";
-           //平面地图
-            string url2 = "http://mt1.google.cn/vt?pb=!1m4!1m3!1i@Z!2i@X!3i@Y!2m3!1e0!2sm!3i323000000!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0";
-           //卫星地图
-            string url3 = "http://mt0.google.cn/vt?lyrs=s@186&hl=zh-Hans-CN&gl=CN&x=@X&y=@Y&z=@Z";
+            
+            
+           ////平面地图
+           // string url2 =  "http://mt1.google.cn/vt?pb=!1m4!1m3!1i@Z!2i@X!3i@Y!2m3!1e0!2sm!3i323000000!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e47!12m3!1e37!2m1!1ssmartmaps!4e0";
+           ////卫星地图
+           // string url3 = "http://mt0.google.cn/vt?lyrs=s@186&hl=zh-Hans-CN&gl=CN&x=@X&y=@Y&z=@Z";
                          
-            //http://mt0.google.cn/vt?lyrs=s@186&hl=zh-Hans-CN&gl=CN&x=210&y=98&z=8&token=94681
-            //卫星地图线路
-            string url4 = "http://mt0.google.cn/vt?pb=!1m5!1m4!1i@Z!2i@X!3i@Y!4i256!2m3!1e0!2sm!3i325000000!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e50!12m3!1e37!2m1!1ssmartmaps!4e0";
-            //地形图
-            string url5 =
-                "http://mt1.google.cn/vt?pb=!1m5!1m4!1i@Z!2i@X!3i@Y!4i256!2m3!1e4!2st!3i132!2m3!1e0!2sr!3i325150060!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e63!12m3!1e37!2m1!1ssmartmaps!4e0";
-            //Zoom zm = zooms1.Find(z => z.level == zoom);
+           // //http://mt0.google.cn/vt?lyrs=s@186&hl=zh-Hans-CN&gl=CN&x=210&y=98&z=8&token=94681
+           // //卫星地图线路
+           // string url4 = "http://mt0.google.cn/vt?pb=!1m5!1m4!1i@Z!2i@X!3i@Y!4i256!2m3!1e0!2sm!3i325000000!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e50!12m3!1e37!2m1!1ssmartmaps!4e0";
+           // //地形图
+           // string url5 =
+           //     "http://mt1.google.cn/vt?pb=!1m5!1m4!1i@Z!2i@X!3i@Y!4i256!2m3!1e4!2st!3i132!2m3!1e0!2sr!3i325150060!3m9!2szh-Hans-CN!3sCN!5e78!12m1!1e63!12m3!1e37!2m1!1ssmartmaps!4e0";
+           // //Zoom zm = zooms1.Find(z => z.level == zoom);
 
-            string url = url2;
+            string url = url1;
             switch (mt)
             {
                 case MapType.pm:
                 {
-                    url = url2;
+                    url = url1;
                 }
                     break;
                 case MapType.wx:
                     {
-                        url = url3;
+                        url = url2;
                     }
                     break;
                 case MapType.wxxl:
                     {
-                        url = url4;
+                        url = url3;
                     }
                     break;
                 case MapType.dx:
                     {
-                        url = url5;
+                        url = url4;
                     }
                     break;
             }
